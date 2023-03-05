40b4109c0941638a3e893564c0c8a7e479999b8d
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-03-05 16:33:31 +0200

    Refactoring

---
+++ src/input.ts
@@ -0,0 +1,23 @@
+import { Input } from 'cliffy/prompt/mod.ts'
+import { ChatCompletionRequestMessage } from 'openai'
+
+import { createCompletion } from './openai/api.ts'
+
+export const getMessage = async () => {
+  const message = await Input.prompt({
+    message: 'Enter your message',
+    minLength: 3,
+  })
+
+  return message
+}
+
+export const getCompletion = async (messages: ChatCompletionRequestMessage[]) => {
+  const data = await createCompletion(messages)
+
+  const response = data?.choices[0].message?.content
+
+  if (!response) throw new Error('No response from OpenAI API')
+
+  return response
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { Command } from 'cliffy/command/mod.ts'
-import { Input } from 'cliffy/prompt/mod.ts'
 import { colors } from 'cliffy/ansi/colors.ts'
 import { renderMarkdown } from 'charmd/mod.ts'
 
-import { createCompletion } from './openai/api.ts'
+import { getMessage, getCompletion } from './input.ts'
 import { getStartMessages, newMessage } from './openai/prompt.ts'
 import logger from './util/logger.ts'
 
@@ -11,26 +10,23 @@ const start = async () => {
   const messages = getStartMessages()
 
   while (true) {
-    const message = await Input.prompt({
-      message: 'Enter your message',
-      minLength: 3,
-    })
-
+    const message = await getMessage()
     messages.push(newMessage(message))
 
-    const data = await createCompletion(messages)
+    const response = await getCompletion(messages)
+    const formattedResponse = colors.white(renderMarkdown(response))
 
-    const response = data?.choices[0].message?.content
-
-    if (!response) throw new Error('No response from OpenAI API')
-
-    logger.info(colors.white(renderMarkdown(response)))
+    logger.info(formattedResponse)
   }
 }
 
-await new Command()
-  .name('codeassistant')
-  .version('0.0.1')
-  .description('OpenAI API powered coding assistant')
-  .action(start)
-  .parse(Deno.args)
+const main = async () => {
+  await new Command()
+    .name('codeassistant')
+    .version('0.0.1')
+    .description('OpenAI API powered coding assistant')
+    .action(start)
+    .parse(Deno.args)
+}
+
+main()
