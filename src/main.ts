fa6464ccf5f97879607d91bf93661d2badbbd316
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-03-05 11:31:06 +0200

    Setup rudimentary CLI with Cliffy

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
+import { Command } from 'cliffy/command/mod.ts'
+import { Input } from "cliffy/prompt/mod.ts"
+
 import { createCompletion } from './util/openai.ts'
 
-if (import.meta.main) await createCompletion('Say this is a test')
+const start = async () => {
+  const prompt = await Input.prompt({
+    message: 'Enter your prompt',
+    minLength: 3,
+  })
+
+  createCompletion(prompt)
+}
+
+await new Command()
+  .name('codeassistant')
+  .version('0.0.1')
+  .description('OpenAI API powered coding assistant')
+  .action(start)
+  .parse(Deno.args)
